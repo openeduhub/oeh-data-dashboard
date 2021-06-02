3668e7ee2b0bb50a522aac96c4d0a8583fb5a85a
@s.roertgen <sroertgen@gmail.com>
2021-06-02 09:05:17 +0200

    sort imports, add loading spinner

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import logging
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash_html_components.H3 import H3
-import logging
-from Collections import Collections
+from dash_core_components.Loading import Loading
+
+from Collections import Collection, Collections
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,29 +23,56 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 suppress_callback_exceptions=True)
 app.title = "WLO Analytics"
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
 
 def build_index_page(C: Collections):
     index_page = html.Div(
         children=[
-            html.Div(className="index-container",
-                    children=[
-                        *C.cards_for_index_page,
-                    ]),
-            # html.H3("Allgemein Übersicht", className="row-header"),
-            # general_fpm_board.layout
+            dcc.Loading(
+            id="loading-1",
+            type="default",
+            fullscreen=True,
+            children=html.Div(
+                id="loading-output-1",
+                children=[
+                    html.Div(className="index-container",
+                            children=[
+                                *C.cards_for_index_page,
+                            ]),
+                    html.Div(
+                        className="info-row-2",
+                        children=[
+                            C.searched_materials_not_in_collections_layout
+                            ]
+                        )
+                    ]
+                )
+            )
         ]
     )
     return index_page
 
+index_page = build_index_page(C)
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dcc.Loading(
+            id="loading-1",
+            type="graph",
+            fullscreen=True,
+            children=[
+                html.Div(
+                    id="loading-output-1",
+                    children=[html.Div(id='page-content')])
+                ]
+            )
+        ]
+    )
+
 
 # Update the index
-@ app.callback(dash.dependencies.Output('page-content', 'children'),
-               [dash.dependencies.Input('url', 'pathname')])
+@ app.callback(
+    dash.dependencies.Output('page-content', 'children'),
+    dash.dependencies.Input('url', 'pathname'))
 def display_page(pathname):
     if pathname in C.pathnames:
         target_collection = next(collection for collection in C.collections if collection.app_url == pathname.removeprefix("/"))
@@ -51,9 +80,7 @@ def display_page(pathname):
         return target_collection.layout
     else:
         return index_page
-    # You could also return a 404 "URL not found" page here
 
 
 if __name__ == "__main__":
-    index_page = build_index_page(C)
     app.run_server(host="0.0.0.0", debug=True, port=8050)
